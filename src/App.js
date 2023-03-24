33c6fa24e0833b1c9eab7a5476b03d9a295b8c2c
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-03-24 18:22:11 +0000

    installing and importing necessary libraries or icons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faCircleCheck, faPen, faTrashCan
+} from '@fortawesome/free-regular-svg-icons'; 
 
 import './App.css';
 
