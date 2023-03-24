80b07784142fcdb1d7981c2c5624a5a894f22401
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-03-24 19:00:02 +0000

    adding dummy data and using useState

--- src/App.js
+++ src/App.js
@@ -8,14 +8,24 @@ import {
 import './App.css';
 
 function App() {
+
+  // Tasks (ToDo List) State
+  const [ToDo, setToDo] = useState([
+    {"id": 1, "title": "Task 1", "status": false},
+    {"id": 2, "title": "Task 2", "status": false}
+  ]);
+
+  // Temp State
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState('');
+
+  
   return (
     <div className="container App">
 
       <br/><br/>
       <h2>To Do List App (ReactJS)</h2>
       <br/><br/>
-      
-      WebStylePress
     </div>
   );
 }
